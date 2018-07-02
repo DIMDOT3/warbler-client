d89228ff113d1e69a39fd4556e4041efac35e437
Joseph Dinh <JDinh1985@gmail.com>
2018-07-02 09:14:05 -0400

    ready to deploy

---
+++ src/components/MessageTimeline.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import MessageList from '../containers/MessageList';
+import UserAside from './UserAside';
+
+const MessageTimeline = props => {
+	return (
+		<div className='row'>
+			<UserAside 
+				profileImageUrl={props.profileImageUrl}
+				username={props.username}
+			/>
+			<MessageList />
+		</div>
+	);
+}
+
+export default MessageTimeline;
+
+
+
+
+
+
